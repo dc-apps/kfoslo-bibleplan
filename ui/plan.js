79d4cb940cfc535119e8926dbeedb4326777956f
DC Apps <dcapps.no@gmail.com>
2013-07-24 16:58:09 +0200

    Made actual bible plan separate file
    Makes it easier for other churches / groups to add other bible plans,
    and makes editing the plan easier for non-developers.

---
+++ ui/plan.js
@@ -0,0 +1,41 @@
+//Bibleplan for KF Oslo (Global function):
+function getReading(date){
+	switch(date){
+        case "20.7":
+        	return "Salme 126<br />Mal 1-2<br />Åpenbaringen 21";     
+        break;
+        case "21.7":
+        	return "Malaki 3-4<br />Åpenbaringen 22";     
+        break;
+        case "22.7":
+        	return "1. Mosebok 1-2<br />Lukas 1,1-25";     
+        break;
+        case "23.7":
+        	return "1. Mosebok 3-5<br />Lukas 1,26-56";     
+        break;
+        case "24.7":
+        	return "1. Mosebok 6-8<br />Lukas 1,57-80";     
+        break;
+        case "25.7":
+        	return "1. Mosebok 9-11<br />Johannes 1,1-14";     
+        break;
+        case "26.7":
+        	return "Job 1-3<br />Matteus 1";     
+        break;
+        case "27.7":
+        	return "Job 4-6<br />Lukas 2,1-20";     
+        break;
+        case "28.7":
+        	return "Job 7-9<br />Lukas 2,21-38";     
+        break;
+        case "29.7":
+        	return "Job 10-13<br />Matteus 2";     
+        break;
+        case "30.7":
+        	return "Job 14-16<br />Lukas 2,39-52";     
+        break;
+        case "31.7":
+        	return "Job 17-30<br />Matteus 3";     
+        break;
+        }
+    } 
